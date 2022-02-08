0761413565180f97d65e2a20692a0d5dcd3a9720
idurar <lalami.sdn@gmail.com>
2022-02-08 13:13:21 +0100

    add new repo page

--- src/components/RepoCard.tsx
+++ src/components/RepoCard.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Card, Typography, Button } from 'antd';
-import { HeartOutlined, StarOutlined, GithubOutlined } from '@ant-design/icons';
+import { HeartOutlined, StarOutlined, GithubOutlined, ArrowsAltOutlined } from '@ant-design/icons';
 
 import { useSelector, useDispatch } from 'react-redux';
-
+import { useNavigate } from 'react-router-dom';
 import { github } from '@/redux/github/actions';
 import { selectFavorList } from '@/redux/github/selectors';
 
@@ -11,9 +11,17 @@ import { repoType } from '@/types';
 
 const { Paragraph } = Typography;
 
-const Action = ({ text, icon }: { text: string; icon: React.ReactNode }) => {
+const Action = ({
+  text,
+  icon,
+  onClick = () => {},
+}: {
+  text: string;
+  icon: React.ReactNode;
+  onClick?: () => void;
+}) => {
   return (
-    <div className="actionContainer">
+    <div className="actionContainer" onClick={onClick}>
       <div className="actionIcon">
         {icon}
         <div className="actionText">{text}</div>
@@ -49,25 +57,34 @@ const FavorIcon = ({ repoId }: { repoId: any }) => {
   );
 };
 
-const RepoCard = ({ item }: { item: repoType }) => {
+const RepoCard = ({ item }: { item: repoType | undefined }) => {
+  let navigate: any = useNavigate();
   return (
     <Card
-      title={item.full_name}
-      extra={<FavorIcon repoId={item.id} />}
+      title={item?.full_name}
+      extra={<FavorIcon repoId={item?.id} />}
       className="repoCard"
       actions={[
         <Action
-          text={item.stargazers_count + (item.stargazers_count > 1 ? ' stars' : ' stars')}
+          text={item?.stargazers_count + (item && item.stargazers_count > 1 ? ' stars' : ' stars')}
           key="star"
           icon={<StarOutlined />}
         />,
-        <Action text={item.language || 'Unknow'} key="language" icon={<GithubOutlined />} />,
+        <Action text={item?.language || 'Unknow'} key="language" icon={<GithubOutlined />} />,
+        <Action
+          text={'Open'}
+          key="language"
+          icon={<ArrowsAltOutlined />}
+          onClick={() => {
+            navigate(`/repository/${item?.id}`);
+          }}
+        />,
       ]}
     >
       <Paragraph ellipsis={{ rows: 2 }}>
-        Link : <a href={item.html_url}>{item.html_url}</a>
+        Link : <a href={item?.html_url}>{item?.html_url}</a>
       </Paragraph>
-      <Paragraph ellipsis={{ rows: 3 }}>Description : {item.description}</Paragraph>
+      <Paragraph ellipsis={{ rows: 3 }}>Description : {item?.description}</Paragraph>
     </Card>
   );
 };
